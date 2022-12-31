5714e59a8c0838580d6ada70d3a94aa742c12b8e
Manisha Chaturvedi <68684840+manisha1904@users.noreply.github.com>
2022-12-31 15:43:19 +0530

    Create Combination_Sum.cpp

---
+++ Microsoft/Combination_Sum.cpp
@@ -0,0 +1,27 @@
+Problem Link - leetcode.com/problems/combination-sum-iii/description/
+
+vector<vector<int>>ans;
+    int sum(vector<int>&v){
+        int s=0;
+        for(auto& x:v)
+            s+=x;
+        return s;
+    }
+    void solve(int k,int n,vector<int>&temp,int num){
+        if(k==0){
+            if(sum(temp)==n)
+                ans.push_back(temp);
+            return;
+        }
+        if(num>9)
+            return;
+        temp.push_back(num);
+        solve(k-1,n,temp,num+1);
+        temp.pop_back();
+        solve(k,n,temp,num+1);
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>temp;
+        solve(k,n,temp,1);
+        return ans;
+    }
