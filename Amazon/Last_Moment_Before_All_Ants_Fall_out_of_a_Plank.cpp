9efca47de3d050600af7fab6875fa06842bc5884
Manisha Chaturvedi <68684840+manisha1904@users.noreply.github.com>
2023-01-30 11:45:34 +0530

    Create Last_Moment_Before_All_Ants_Fall_out_of_a_Plank.cpp

---
+++ Amazon/Last_Moment_Before_All_Ants_Fall_out_of_a_Plank.cpp
@@ -0,0 +1,12 @@
+// Problem Link - https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/description/
+
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        sort(left.begin(),left.end());
+        sort(right.begin(),right.end());
+        if(left.empty())return n-right[0];
+        if(right.empty())return left[left.size()-1];
+        return max(left[left.size()-1],n-right[0]);
+    }
+};
